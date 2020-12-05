aa917a7fdf4d1babf87d0e83f763d62c79141741
Orinxlm <w.spicerx@gmail.com>
2020-12-04 20:51:54 -0500

    some defines

--- lex.c
+++ lex.c
@@ -67,3 +67,18 @@ void recognize_integer(void) {
     Token._class = INTEGER; next_char();
     while(is_digit(input_char)) {next_char();}
 }
+
+#define is_end_of_input(ch) ((ch) == '\0')
+#define is_layout(ch) (!is_end_of_input(ch) && (ch) <= ' ')
+#define is_comment_starter(ch) ((ch) == '#')
+#define is_comment_stopper(ch) ((ch) == "#" || (ch) == '\n')
+
+#define is_uc_letter(ch) ('A' <= (ch) && (ch) <= 'Z')
+#define is_lc_letter(ch) ('a' <= (ch) && (ch) <= 'z')
+#define is_letter(ch) (is_uc_letter(ch) || is_lc_letter(ch))
+#define is_digit(ch) ('0' <= (ch) && (ch) <= '9')
+#define is_letter_or_digit(ch) ((is_letter(ch) || is_digit(ch)))
+#define is_underscore(ch) ((ch) == '_')
+
+#define is_operator(ch) (strchr("+-*/", (ch) != 0)
+#define is_separator(ch) (strchr(" ;,(){} ", (ch) != 0))
