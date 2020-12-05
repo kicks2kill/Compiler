db947fde9617e776d44832450db217d4c2a00c37
Orinxlm <w.spicerx@gmail.com>
2020-12-05 15:30:40 -0500

    adding return values and starting arguments

---
+++ args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lex.h"
+
+void factor     (char *tempvar);
+void term       (char *tempvar);
+void expression (char *tempvar);
+
+extern char *newname    (void);
+extern void freename    (char *name);
+
+statements()
+{
+    //TODO
+
+}
