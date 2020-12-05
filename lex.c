8259edbae15c6dccdc8931cefbf1038b76362a6e
Orinxlm <w.spicerx@gmail.com>
2020-12-05 11:17:24 -0500

    adding lookahead token

--- lex.c
+++ lex.c
@@ -2,19 +2,25 @@
 #include <stdio.h>
 #include <ctype.h>
 
-char *yytext   = ""; /* our Lexeme (not '\0' terminated) */
+char *yytext   = ""; /* current Lexeme (not '\0' terminated) */
 int   yyleng   = 0;/* Lexeme length*/
 int   yylineno = 0; /*Input line number*/
 
 lex()
 {
+    /*
+     * Lexical Anaylzers often need to know what the next input character is going to be without actually reading past it.
+     * They must 'look ahead' by some number of characters.
+     * Similarly, they often need to read past the end of a lexeme in order to recognize and then push back the unnecessary characters onto the input stream.  
+     * Consequently, there are often extra characters that must be handled specially.    
+    */
     static char input_buffer[128];
     char *current;
 
-    current = yytext + yyleng; //skip current Lexeme
+    current = yytext + yyleng; //init to empty string
     while(1)
     {
-        while(!*current)
+        while(!*current)       //return true
         {
             /*
                 Get new lines, skipping any leading white space on the line, until a nonblank line is found
@@ -62,4 +68,22 @@ lex()
             }
         }
     }
+}
+
+static int Lookahead = -1; //lookahead token
+
+int match(token)
+int token;
+{
+    //Return true if "token" matches the current lookahead symbol
+    if( Lookahead == -1)
+        Lookahead = lex();
+    
+    return token == Lookahead;
+}
+
+void advance()
+{
+    //advance the lokoahead to next input symbol
+    Lookahead = lex();
 }
