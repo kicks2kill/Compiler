383d52c39d7be22b38159bc2c75b81b80be9c91b
Orinxlm <w.spicerx@gmail.com>
2020-12-04 22:29:27 -0500

    redoing the lexical analyzer

---
+++ lex.c
@@ -0,0 +1,65 @@
+#include "lex.h"
+#include <stdio.h>
+#include <ctype.h>
+
+char *yytext   = ""; /* our Lexeme (not '\0' terminated) */
+int   yyleng   = 0;/* Lexeme length*/
+int   yylineno = 0; /*Input line number*/
+
+lex()
+{
+    static char input_buffer[128];
+    char *current;
+
+    current = yytext + yyleng; //skip current Lexeme
+    while(1)
+    {
+        while(!*current)
+        {
+            /*
+                Get new lines, skipping any leading white space on the line, until a nonblank line is found
+            */
+
+           current = input_buffer;
+           if(!gets(input_buffer))
+           {
+               *current = '\0';
+               return EOI;
+           }
+           ++yylineno;
+           while( isspace(*current))
+                ++current;
+        }
+        for( ; *current; ++current)
+        {
+            /* get next token */
+            yytext = current;
+            yyleng = 1;
+
+            switch(*current)
+            {
+                case EOF: return EOI    ;
+                case ';': return SEMI   ;
+                case '+': return PLUS   ;
+                case '*': return TIMES  ;
+                case '(': return LP     ;
+                case ')': return RP     ;
+
+                case '\n':
+                case '\t':
+                case ' ':break;
+                default:
+                    if(!isalnum(*current))
+                        fprintf(stderr, "Ignoring Illegal input <%c>\n", *current);
+                    else
+                    {
+                        while(isalnum(*current))
+                            ++current;
+                        yyleng = current - yytext;
+                        return NUM_OR_ID;
+                    }
+                    break;
+            }
+        }
+    }
+}
