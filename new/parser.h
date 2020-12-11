df56d09a7c8297934ba463265630d73b9871a752
Orinxlm <w.spicerx@gmail.com>
2020-12-11 10:03:25 -0500

    correcting some errors

--- new/parser.h
+++ new/parser.h
@@ -1,16 +1,27 @@
 #include <stdlib.h>
 #include "lex.h"
-#include "parser.h" 
+
+typedef int Operator;
+typedef struct _expression {
+    char type;
+    int value;
+    struct _expression *left, *right;
+    Operator oper;
+} Expression;
+
+typedef Expression AST_Node;
+
+extern int Parse_program(AST_Node **);
 
 static Expression *new_expression(void){
     return (Expression*)malloc( sizeof( Expression ));
 }
 
-static void free_expression(Expression *expr) { free(( void * )) expr ); }
+static void free_expression(Expression *expr) { free(( void * )expr ); }
 static int Parse_operator(Operator *oper_p);
 static int Parse_expression(Expression **expr_p);
 
-int Parse_program(AST_node **icode_p) {
+int Parse_program(AST_Node **icode_p) {
     Expression *expr;
     get_next_token();
     if( Parse_expression( &expr )) {
@@ -22,3 +33,4 @@ int Parse_program(AST_node **icode_p) {
     } 
     return 0;
 }
+
